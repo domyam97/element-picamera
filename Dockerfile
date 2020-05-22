32474c40553f9474fd66c609b1e6edba4eea73bc
domyam97 <domyam97@gmail.com>
2020-05-22 12:48:42 -0700

    working with element-streamviewer

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM elementaryrobotics/atom:opencv-aarch64
 
 # Install sudo function
-RUN apt-get install sudo cmake build-essential -y
+#RUN apt-get install sudo -y
 
 # Lets copy over the contents of this repo into the container
-ADD ./third-party/userland /code/third-party/userland
+#ADD ./third-party/userland /code/third-party/userland
 ADD . /code
 
 # install python dependencies
@@ -12,18 +12,23 @@ RUN pip3 install wheel
 RUN pip3 install -r /code/requirements.txt
 
 # try to compile userland
-WORKDIR /code/third-party/userland
-RUN chmod +x buildme
-RUN ./buildme --aarch64
+#WORKDIR /code/third-party/userland
+#RUN chmod +x buildme
+#RUN ./buildme --aarch64
 
-RUN export PATH=$PATH:/opt/vc/bin && echo $PATH
-#RUN export LD_LIBRARY_PATH=$LD_LIBRARY_PATH/opt/vc/lib && echo $LD_LIBRARY_PATH
+
+ARG opt_libs
+ENV PATH="/opt/vc/bin:${PATH}" 
+RUN echo $PATH
+
+ENV LD_LIBRARY_PATH="$opt_libs:${LD_LIBRARY_PATH}"
+RUN echo $LD_LIBRARY_PATH
 
 RUN ldconfig
 
 WORKDIR /code
 RUN chmod +x launch.sh
-CMD ["/bin/bash", "launch.sh"]
+CMD ["/bin/sh", "launch.sh"]
 
 
 
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from time import sleep
 from atom import Element
-from atom.messages import Response
+from atom.messages import Response, LogLevel
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 import numpy as np
@@ -12,7 +12,7 @@ import sys
 
 sys.path.append(os.path.abspath("/code/include"))
 
-from contracts import ColorStreamContract
+from contracts import ColorStreamContract, PICAMERA_ELEMENT_NAME
 
 class Picamera:
     
@@ -33,17 +33,17 @@ class Picamera:
         # Run command loop
         thread = Thread(target=self._element.command_loop, daemon=True)
         thread.start()
-
+    
     def is_healthy(self):
-        """
-        Reports whether the camera is connected or not
-        """
+        # Reports whether the camera is connected or not
         try:
            self._status_lock.acquire()
            if self._status_is_running:
                return Response(err_code=0, err_str="Camera is good")
            else:
                return Response(err_code=1, err_str="Camera is not good")
+        except:
+           return Response(err_code=0, err_str="Could not reach thread")
 
     def run_camera_stream(self):
         while True:
@@ -51,7 +51,7 @@ class Picamera:
                 # try to open up camera
                 self._element.log(LogLevel.INFO, "Opening PiCamera")
                 self._camera = PiCamera()
-                self._color-array = PiRGBArray(self._camera)
+                self._color_array = PiRGBArray(self._camera)
                 
                 # set camera configs
                 self._camera.resolution = (self._width, self._height)
@@ -65,17 +65,17 @@ class Picamera:
                     self._status_is_running = True
                 finally:
                     self._status_lock.release()
-
+                
                 self._element.log(LogLevel.INFO, "Picamera connected and streaming")
-
+                
                 while True:
                     start_time = time.time()
-                
-                    self._camera.capture(self._color-array, format = 'bgr')
-                    color_image = self._color-array.array
+                    
+                    self._camera.capture(self._color_array, format = 'bgr')
+                    color_image = self._color_array.array
                     
                     #do some rotation here
-
+                    
                     _, color_serialized = cv2.imencode(".tif", color_image)
                     
                     color_contract = ColorStreamContract(data=color_serialized.tobytes())
@@ -86,6 +86,7 @@ class Picamera:
                             maxlen=self._fps
                     )
                     time.sleep(max(1 / self._fps - (time.time() - start_time),0))
+                    self._color_array.truncate(0)
             except:
                 self._element.log(LogLevel.INFO, "Camera threw exception: %s" % (sys.exc_info()[1]))
 
@@ -93,6 +94,7 @@ class Picamera:
                 try:
                     self._status_lock.acquire()
                     self._status_is_running = False
+                    self._camera.close()
                 finally: 
                     self._status_lock.release()
 
@@ -104,7 +106,7 @@ if __name__ == "__main__":
     height = int(os.getenv('HEIGHT','720'))
     fps = int(os.getenv('FPS','30'))
     rotation = os.getenv('ROTATION', None)
-    retry_delay = float(os.getenv('RETRY_DELAY', '3.0'))
+    retry_delay = float(os.getenv('RETRY_DELAY', '1.0'))
     
     if rotation is not None:
         try:
@@ -115,10 +117,10 @@ if __name__ == "__main__":
             raise ValueError("Rotation must be an integer that is a multiple of 90")
     
     camera = Picamera(
-            element_name=element_name
-            width=width
-            height=height
-            fps=fps
+            element_name=element_name,
+            width=width,
+            height=height,
+            fps=fps,
             retry_delay=retry_delay)
     
     camera.run_camera_stream()
